cf6de923da632d7b222fae5053066148a2cf214a
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-08 23:05:16 +0000

    Add SqlConnectionWrapper interfaces

---
+++ src/AuthenticationProject.Shared/Interfaces/ConnectionWrappers/ISqlConnectionWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthenticationProject.Shared.Interfaces.ConnectionWrappers
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public interface ISqlConnectionWrapper : IDisposable
+    {
+        SqlConnection SqlConnection { get; }
+    }
+}
