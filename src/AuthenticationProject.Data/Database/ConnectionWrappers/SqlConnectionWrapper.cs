2e3814c5a22803a17c9a6c11b78e4852becf9e3c
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-09 22:05:44 +0000

    Implement SqlConnectionWrapper concrete implementation

---
+++ src/AuthenticationProject.Data/Database/ConnectionWrappers/SqlConnectionWrapper.cs
@@ -0,0 +1,26 @@
+﻿namespace AuthenticationProject.Data.Database.ConnectionWrappers
+{
+    using System.Data;
+    using System.Data.SqlClient;
+    using Shared.Interfaces.ConnectionWrappers;
+
+    public class SqlConnectionWrapper : ISqlConnectionWrapper, ISecuritySqlConnectionWrapper
+    {
+        public SqlConnectionWrapper(string connectionString)
+        {
+            SqlConnection = new SqlConnection(connectionString);
+
+            SqlConnection.Open();
+        }
+
+        public SqlConnection SqlConnection { get; }
+
+        public void Dispose()
+        {
+            if (SqlConnection.State == ConnectionState.Open)
+            {
+                SqlConnection.Close();
+            }
+        }
+    }
+}
