85f3ffdd2e5e31ec929654fec76daab90e650f22
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-10 11:34:44 +0000

    Added UserResult class

---
+++ src/AuthenticationProject.Shared/Results/UserResult.cs
@@ -0,0 +1,30 @@
+﻿namespace AuthenticationProject.Shared.Results
+{
+    using System.Collections.Generic;
+
+    public class UserResult
+    {
+        public UserResult()
+        {
+            ValidationMessages = new Dictionary<string, string>();
+        }
+
+        public UserResult(bool actionSucceeded)
+        {
+            ActionSucceeded = actionSucceeded;
+
+            ValidationMessages = new Dictionary<string, string>();
+        }
+
+        public bool ActionSucceeded { get; private set; }
+
+        public void AddValidationMessage(string key, string validationMessage)
+        {
+            ActionSucceeded = false;
+
+            ValidationMessages.Add(key, validationMessage);
+        }
+
+        public Dictionary<string, string> ValidationMessages { get; }
+    }
+}
