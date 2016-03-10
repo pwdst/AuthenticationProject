fb1e5d96f247a35b82598fcade75a84afe28fbe9
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-10 16:00:15 +0000

    Add UserExtensions helper class (static extension methods are not yet support by C#)

---
+++ src/AuthenticationProject.Data/Extensions/Security/UserExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace AuthenticationProject.Data.Extensions.Security
+{
+    using System.Security.Cryptography;
+
+    internal static class UserExtensions
+    {
+        internal static byte[] GenerateSalt(int saltLength = 32)
+        {
+            using (var randomNumberGenerator = RandomNumberGenerator.Create())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        internal static bool PasswordHashesEqual(byte[] providedHash, byte[] savedHash)
+        {
+            if (providedHash == null && savedHash == null)
+            {
+                return true;
+            }
+            if (providedHash == null || savedHash == null || providedHash.Length != savedHash.Length)
+            {
+                return false;
+            }
+
+            var bytesMatch = true;
+
+            for (var i = 0; i < providedHash.Length; i++)
+            {
+                bytesMatch &= (providedHash[i] == savedHash[i]);
+            }
+
+            return bytesMatch;
+        }
+
+        internal static byte[] HashPassword(byte[] passwordBytes, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(passwordBytes, salt, numberOfRounds))
+            {
+                return rfc2898.GetBytes(32);
+            }
+        }
+    }
+}
