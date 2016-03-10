a431893bbc90d31bfb9b014bc3c12b012397060f
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-10 11:28:15 +0000

    Add User Entity

---
+++ src/AuthenticationProject.Data/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿namespace AuthenticationProject.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Shared.Projections;
+
+    internal class User : IUser
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        [DataType(DataType.EmailAddress), Required]
+        public string EmailAddress { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string Salt { get; set; }
+
+        public string TwitterUserName { get; set; }
+    }
+}
---
+++ src/AuthenticationProject.Shared/Projections/User.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthenticationProject.Shared.Projections
+{
+    using System;
+
+    public interface IUser
+    {
+        Guid Id { get; }
+
+        string UserName { get; }
+
+        string DisplayName { get; }
+
+        string EmailAddress { get; }
+
+        string TwitterUserName { get; }
+    }
+}
