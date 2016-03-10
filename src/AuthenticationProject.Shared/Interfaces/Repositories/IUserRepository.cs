02baa4ed4ae728116d76a7fb240699c418e03fe1
Peter Stone-Thompson <github@peterstonethompson.org.uk>
2016-03-10 12:00:14 +0000

    Add IUserRepository user repository method definition

--- src/AuthenticationProject.Shared/Interfaces/Repositories/IUserRepository.cs
+++ src/AuthenticationProject.Shared/Interfaces/Repositories/IUserRepository.cs
@@ -1,6 +1,15 @@
 ﻿namespace AuthenticationProject.Shared.Interfaces.Repositories
 {
+    using System;
+    using JetBrains.Annotations;
+    using Results;
+
     public interface IUserRepository
     {
+        UserResult CreateUser([NotNull] string userName, [NotNull] string displayName, [NotNull] string emailAddress, [NotNull] string password, string twitterUserName);
+
+        UserResult UpdatePassword(Guid userId, [NotNull] string existingPassword, [NotNull] string newPassword);
+
+        UserResult ValidatePassword([NotNull] string username, string password);
     }
 }
